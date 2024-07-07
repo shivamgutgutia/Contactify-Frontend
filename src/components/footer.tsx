0f7b3f430ab9a5534ddc5643f5cc48065679b4bb
Shivam Gutgutia <100862487+shivamgutgutia@users.noreply.github.com>
2024-07-07 16:26:48 +0530

    Update footer.tsx
    Add: Link to GitHub repository

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="mt-auto flex w-full items-center justify-between border-t px-8 py-4 text-center">
       <div className="mx-auto flex text-center">
-        Made by Shivam Gutgutia
+        <a href="https://github.com/shivamgutgutia/contactify">GitHub Repository</a>
       </div>
     </div>
   );
